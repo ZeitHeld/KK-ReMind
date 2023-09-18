e58ccb8166c1ca8f531bbd1c3b719712e1d1e842
Xephiro9999 <dreadbot8001@gmail.com>
2023-09-18 14:48:43 -0400

    Next Spells Planning

--- src/main/java/online/magicksaddon/magicsaddonmod/magic/ModMagicks.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/magic/ModMagicks.java
@@ -21,6 +21,9 @@ public class ModMagicks {
             HOLY = MAGIC.register("magic_holy",() -> new magicHoly(new ResourceLocation(MagicksAddonMod.MODID, "magic_holy"), false, 3, null, order++)),
             RUIN = MAGIC.register("magic_ruin", () -> new magicRuin(new ResourceLocation(MagicksAddonMod.MODID, "magic_ruin"), false, 3, null, order++)),
             BALLOON = MAGIC.register("magic_balloon", () -> new magicBalloon(new ResourceLocation(MagicksAddonMod.MODID, "magic_balloon"), false, 3, order++));
+            //COMET = MAGIC.register("magic_comet", () -> new magicBalloon(new ResourceLocation(MagicksAddonMod.MODID, "magic_comet"), false, 2, order++));
+            //ULTIMA = MAGIC.register("magic_ultima", () -> new magicBalloon(new ResourceLocation(MagicksAddonMod.MODID, "magic_ultima"), false, 1, order++));
+
 
 
 
---
+++ src/main/java/online/magicksaddon/magicsaddonmod/magic/magicUltima.java
@@ -0,0 +1,4 @@
+package online.magicksaddon.magicsaddonmod.magic;
+
+public class magicUltima {
+}
