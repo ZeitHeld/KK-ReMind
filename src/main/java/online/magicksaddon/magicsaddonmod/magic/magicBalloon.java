87c64a38d3ff3f2207ae7f6923fd8da5530306ea
Xephiro9999 <dreadbot8001@gmail.com>
2023-10-02 17:50:39 -0400

    Future Proof

--- src/main/java/online/magicksaddon/magicsaddonmod/magic/magicBalloon.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/magic/magicBalloon.java
@@ -3,6 +3,7 @@ package online.magicksaddon.magicsaddonmod.magic;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrowableProjectile;
 import online.kingdomkeys.kingdomkeys.magic.Magic;
