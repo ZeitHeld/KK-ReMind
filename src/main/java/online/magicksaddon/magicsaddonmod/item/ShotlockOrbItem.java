833c8051aef0cd994ec8e969ba280b1cc256b128
Xephiro9999 <dreadbot8001@gmail.com>
2024-01-22 15:44:59 -0500

    Shotlock Orb pt.1

---
+++ src/main/java/online/magicksaddon/magicsaddonmod/item/ShotlockOrbItem.java
@@ -0,0 +1,51 @@
+package online.magicksaddon.magicsaddonmod.item;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import online.kingdomkeys.kingdomkeys.capability.IPlayerCapabilities;
+import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
+import online.kingdomkeys.kingdomkeys.shotlock.Shotlock;
+import online.magicksaddon.magicsaddonmod.MagicksAddonMod;
+import online.magicksaddon.magicsaddonmod.lib.StringsX;
+import online.magicksaddon.magicsaddonmod.shotlock.AddonShotlocks;
+
+import static net.minecraft.world.ContainerHelper.takeItem;
+
+public class ShotlockOrbItem extends Item {
+    String shotlocks;
+
+    public ShotlockOrbItem(Properties properties, String name){
+        super(properties);
+        this.shotlocks = name;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand){
+        IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
+        Shotlock shotlockInstance = AddonShotlocks.registry.get().getValue(new ResourceLocation(shotlocks));
+
+        if(!world.isClientSide){
+            if (!playerData.getShotlockList().contains(shotlocks)){
+                playerData.addShotlockToList(shotlocks, true);
+                takeItem(player);
+                player.displayClientMessage(Component.translatable("Unlocked " + shotlocks, true);
+            }
+        }
+        return InteractionResultHolder.success(player.getItemInHand(hand));
+    }
+
+    private void takeItem(Player player) {
+        if (!ItemStack.matches(player.getMainHandItem(), ItemStack.EMPTY) && player.getMainHandItem().getItem() == this) {
+            player.getMainHandItem().shrink(1);
+        } else if (!ItemStack.matches(player.getOffhandItem(), ItemStack.EMPTY) && player.getOffhandItem().getItem() == this) {
+            player.getOffhandItem().shrink(1);
+        }
+    }
+
+}
--- src/main/java/online/magicksaddon/magicsaddonmod/shotlock/AddonShotlocks.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/shotlock/AddonShotlocks.java
@@ -3,16 +3,22 @@ package online.magicksaddon.magicsaddonmod.shotlock;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.IForgeRegistry;
 import online.kingdomkeys.kingdomkeys.KingdomKeys;
 import online.kingdomkeys.kingdomkeys.shotlock.Shotlock;
 import online.magicksaddon.magicsaddonmod.MagicksAddonMod;
 import online.magicksaddon.magicsaddonmod.lib.StringsX;
 
+import java.util.function.Supplier;
+
 public class AddonShotlocks {
 
     public static DeferredRegister<Shotlock> SHOTLOCKS = DeferredRegister.create(new ResourceLocation(KingdomKeys.MODID, "shotlocks"), MagicksAddonMod.MODID);
+    
+    public static Supplier<IForgeRegistry<Shotlock>> registry = SHOTLOCKS.makeRegistry(RegistryBuilder::new);
 
     static int order = 100;
+
     public static final RegistryObject<Shotlock>
     	FLAME_SALVO = SHOTLOCKS.register(StringsX.flameSalvo, () -> new ShotlockFlameSalvo(MagicksAddonMod.MODID + ":" + StringsX.flameSalvo, order++, 2, 15)),
         BUBBLE_BLASTER = SHOTLOCKS.register(StringsX.bubbleBlaster, () -> new ShotlockBubbleBlaster(MagicksAddonMod.MODID + ":" + StringsX.bubbleBlaster, order++, 2, 15)),
