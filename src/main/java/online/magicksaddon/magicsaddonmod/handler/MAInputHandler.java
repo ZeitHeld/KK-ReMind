a0f8075bdaab2e25ab982e6228dc88faedcf0da3
Abelatox <abel54225@gmail.com>
2024-03-30 19:31:17 +0100

    Fixed quick run with ReMind

--- src/main/java/online/magicksaddon/magicsaddonmod/handler/MAInputHandler.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/handler/MAInputHandler.java
@@ -701,66 +701,64 @@ public class MAInputHandler extends InputHandler{
 
         // 0.13000001 - Sprint speed (Vanilla)
 
-        // Light/Dark Step Abilities
-        if (qrCooldown <= 0 && (player.getDeltaMovement().x != 0 && player.getDeltaMovement().z != 0)) {
-            if (player.isSprinting()){
+		// Light/Dark Step Abilities
+		if (qrCooldown <= 0 && (player.getDeltaMovement().x != 0 && player.getDeltaMovement().z != 0)) {
+			if (player.isSprinting()) {
 
-                //System.out.println(playerData.getActiveDriveForm());
-                int lightLevel = playerData.getDriveFormLevel("magicksaddon:form_light");
-                int darkLevel = playerData.getDriveFormLevel("magicksaddon:form_dark");
+				// System.out.println(playerData.getActiveDriveForm());
+				int lightLevel = playerData.getDriveFormLevel("magicksaddon:form_light");
+				int darkLevel = playerData.getDriveFormLevel("magicksaddon:form_dark");
 
+				// System.out.println(globalData.getStepTicks());
 
-                //System.out.println(globalData.getStepTicks());
+				// Light Step
+				if (playerData.isAbilityEquipped(StringsX.lightStep) || playerData.getActiveDriveForm().equals("magicksaddon:form_light")) {
+					float yaw = player.getYRot();
+					float motionX = -Mth.sin(yaw / 180.0f * (float) Math.PI);
+					float motionZ = Mth.cos(yaw / 180.0f * (float) Math.PI);
+					double power = lightLevel;
+					PacketHandlerX.sendToServer(new CSSetStepTicksPacket(10, StringsX.lightStepType));
 
-                // Light Step
-                if (playerData.isAbilityEquipped(StringsX.lightStep) || playerData.getActiveDriveForm().equals("magicksaddon:form_light")){
-                    float yaw = player.getYRot();
-                    float motionX = -Mth.sin(yaw / 180.0f * (float) Math.PI);
-                    float motionZ = Mth.cos(yaw / 180.0f * (float) Math.PI);
-                    double power = lightLevel;
-                    PacketHandlerX.sendToServer(new CSSetStepTicksPacket(10, StringsX.lightStepType));
-                    //player.level.playSound(player, player.blockPosition(), MagicSounds.LIGHTSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
+					// Light Form
+					if (playerData.getActiveDriveForm().equals("magicksaddon:form_light")) {
+						player.level().playSound(player, player.blockPosition(), MagicSounds.LIGHTSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
+						player.push(motionX * power / 1.5, 0, motionZ * power / 1.5);
+						qrCooldown = 20;
+					} else if (playerData.isAbilityEquipped(StringsX.lightStep)) {
+						if (lightLevel > 2) {
+							player.level().playSound(player, player.blockPosition(), MagicSounds.LIGHTSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
+							power = lightLevel - 2;
+							player.push(motionX * power, 0, motionZ * power);
+							qrCooldown = 20;
+						}
+					}
+				} else if (playerData.isAbilityEquipped(StringsX.darkStep) || playerData.getActiveDriveForm().equals("magicksaddon:form_dark")) {
+					float yaw = player.getYRot();
+					float motionX = -Mth.sin(yaw / 180.0f * (float) Math.PI);
+					float motionZ = Mth.cos(yaw / 180.0f * (float) Math.PI);
+					double power = darkLevel;
+					PacketHandlerX.sendToServer(new CSSetStepTicksPacket(10, StringsX.darkStepType));
+					// Dark Mode
+					if (playerData.getActiveDriveForm().equals("magicksaddon:form_dark")) {
+						player.level().playSound(player, player.blockPosition(), MagicSounds.DARKSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
+						player.push(motionX * power / 1.5, 0, motionZ * power / 1.5);
+						qrCooldown = 20;
+					} else if (playerData.isAbilityEquipped(StringsX.darkStep)) {
+						if (darkLevel > 2) {
+							player.level().playSound(player, player.blockPosition(), MagicSounds.DARKSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
+							power = darkLevel - 2;
+							player.push(motionX * power, 0, motionZ * power);
+							qrCooldown = 20;
+						}
+					}
 
-                    // Light Form
-                    if (playerData.getActiveDriveForm().equals("magicksaddon:form_light")){
-                        player.level().playSound(player, player.blockPosition(), MagicSounds.LIGHTSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
-                        player.push(motionX * power / 1.5, 0, motionZ * power / 1.5);
-                        qrCooldown = 20;
-                    } else if (playerData.isAbilityEquipped(StringsX.lightStep)) {
-                        if (lightLevel > 2){
-                            player.level().playSound(player, player.blockPosition(), MagicSounds.LIGHTSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
-                            power = lightLevel - 2;
-                            player.push(motionX * power, 0, motionZ * power);
-                            qrCooldown = 20;
-                        }
-                    }
-                }
-                else if (playerData.isAbilityEquipped(StringsX.darkStep) || playerData.getActiveDriveForm().equals("magicksaddon:form_dark")){
-                    float yaw = player.getYRot();
-                    float motionX = -Mth.sin(yaw / 180.0f * (float) Math.PI);
-                    float motionZ = Mth.cos(yaw / 180.0f * (float) Math.PI);
-                    double power = darkLevel;
-                    PacketHandlerX.sendToServer(new CSSetStepTicksPacket(10, StringsX.darkStepType));
-                    // Dark Mode
-                    if (playerData.getActiveDriveForm().equals("magicksaddon:form_dark")){
-                        player.level().playSound(player, player.blockPosition(), MagicSounds.DARKSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
-                        player.push(motionX * power / 1.5, 0, motionZ * power /1.5);
-                        qrCooldown = 20;
-                    } else if (playerData.isAbilityEquipped(StringsX.darkStep)){
-                        if (darkLevel >2) {
-                            player.level().playSound(player, player.blockPosition(), MagicSounds.DARKSTEP1.get(), SoundSource.PLAYERS, 1F, 1F);
-                            power = darkLevel -2;
-                            player.push(motionX * power, 0, motionZ * power);
-                            qrCooldown = 20;
-                        }
-                    }
-
-
-                }
-                PacketHandlerX.syncGlobalToAllAround(player, globalData);
-                //PacketHandlerX.sendToServer(new CSSetStepTicksPacket());
-            }
-        }
+				} else { //If not doing adrk or light step run base KK code
+					super.commandAction();
+				}
+				PacketHandlerX.syncGlobalToAllAround(player, globalData);
+				// PacketHandlerX.sendToServer(new CSSetStepTicksPacket());
+			}
+		}
     }
 
 	public Keybinds getPressedKey() {
