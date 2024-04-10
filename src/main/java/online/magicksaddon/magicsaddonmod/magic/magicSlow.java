f73cde3f972d4befe6f250fc142794d76f4a38b5
Abelatox <abel54225@gmail.com>
2024-04-10 21:45:00 +0200

    Fixed slow

--- src/main/java/online/magicksaddon/magicsaddonmod/magic/magicSlow.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/magic/magicSlow.java
@@ -10,16 +10,11 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.kingdomkeys.kingdomkeys.lib.Party;
 import online.kingdomkeys.kingdomkeys.lib.Party.Member;
 import online.kingdomkeys.kingdomkeys.magic.Magic;
-import online.kingdomkeys.kingdomkeys.network.PacketHandler;
-import online.kingdomkeys.kingdomkeys.network.stc.SCSyncGlobalCapabilityPacket;
 import online.magicksaddon.magicsaddonmod.capabilities.IGlobalCapabilitiesRM;
 import online.magicksaddon.magicsaddonmod.capabilities.ModCapabilitiesRM;
 import online.magicksaddon.magicsaddonmod.client.sound.ModSoundsRM;
@@ -28,8 +23,6 @@ public class magicSlow extends Magic {
 	public magicSlow(ResourceLocation registryName, boolean hasToSelect, int maxLevel) {
 		super(registryName, hasToSelect, maxLevel, null);
 	}
-
-	IGlobalCapabilitiesRM globalData;
 
 	@Override
 	protected void magicUse(Player player, Player caster, int level, float fullMPBlastMult, LivingEntity lockOnTarget) {
@@ -47,23 +40,14 @@ public class magicSlow extends Magic {
 		if (!list.isEmpty()) {
 			for (int i = 0; i < list.size(); i++) {
 				Entity e = (Entity) list.get(i);
-				if (e instanceof LivingEntity) {
+				if (e instanceof LivingEntity lEntity) {
+					IGlobalCapabilitiesRM globalData = ModCapabilitiesRM.getGlobal(lEntity);
 					if (globalData != null) {
-						if (e instanceof Mob) {
-							IGlobalCapabilitiesRM globalData = ModCapabilitiesRM.getGlobal((LivingEntity) e);
-							//((Mob) e).getAttribute(Attributes.MOVEMENT_SPEED).addTransientModifier(new AttributeModifier("Slow", -(0.25 + (0.25 * level)), AttributeModifier.Operation.MULTIPLY_BASE));
-							//((Mob) e).getAttribute(Attributes.ATTACK_SPEED).addTransientModifier(new AttributeModifier("Slow", -(0.25 + (0.25 * level)), AttributeModifier.Operation.MULTIPLY_BASE));
-							((Mob) e).addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,time, level + 1));
-							((Mob) e).addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN,time, level + 1));
-						}
+						lEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,time, level + 1));
+						lEntity.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN,time, level + 1));
+System.out.println(time);
 						globalData.setSlowTicks(time, level); // Slow Time
 						globalData.setSlowCaster(player.getDisplayName().getString());
-						if (e instanceof ServerPlayer) {
-							IGlobalCapabilitiesRM globalData = ModCapabilitiesRM.getGlobal((LivingEntity) e);
-							((ServerPlayer) e).addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN,time, level + 1));
-							((ServerPlayer) e).addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN,time, level + 1));
-							//PacketHandler.sendTo(new SCSyncGlobalCapabilityPacket(), (ServerPlayer) e);
-						}
 					}
 				}
 			}
