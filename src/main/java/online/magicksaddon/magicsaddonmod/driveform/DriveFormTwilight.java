e7f99feba069fc0639022d3d8cce9c7a9237cfa2
Abelatox <abel54225@gmail.com>
2024-04-23 19:32:47 +0200

    work on twilight form

--- src/main/java/online/magicksaddon/magicsaddonmod/driveform/DriveFormTwilight.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/driveform/DriveFormTwilight.java
@@ -1,17 +1,21 @@
 package online.magicksaddon.magicsaddonmod.driveform;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.fml.common.Mod;
+import online.kingdomkeys.kingdomkeys.capability.IPlayerCapabilities;
+import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.kingdomkeys.kingdomkeys.driveform.DriveForm;
+import online.kingdomkeys.kingdomkeys.item.ModItems;
 import online.magicksaddon.magicsaddonmod.KingdomKeysReMind;
-import online.magicksaddon.magicsaddonmod.mixin.DriveFormMixin;
+import online.magicksaddon.magicsaddonmod.lib.StringsRM;
 
 @Mod.EventBusSubscriber(modid = KingdomKeysReMind.MODID)
 public class DriveFormTwilight extends DriveForm {
     ResourceLocation skinRL2;
     public DriveFormTwilight(String registeryName, int order, ResourceLocation skinRL, boolean hasKeychain, boolean baseGrowthAbilities) {
         super(registeryName, order, hasKeychain, baseGrowthAbilities);
-        ((DriveFormMixin) this).setColor(new float[] {0.25F,0.25F,0.25F});
+        this.color = new float[] {0.25F,0.25F,0.25F};
         skinRL2 = skinRL;
     }
 
@@ -20,5 +24,22 @@ public class DriveFormTwilight extends DriveForm {
     @Override
     public ResourceLocation getTextureLocation() {
         return skinRL2;
+    }
+    
+    @Override
+    public boolean isSlotVisible(Player player) {
+    	IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
+    	if (playerData != null) {
+			if (playerData.getDriveFormLevel(KingdomKeysReMind.MODID + ":" + StringsRM.darkMode) == 7 && playerData.getDriveFormLevel(KingdomKeysReMind.MODID + ":" + StringsRM.light) == 7) {
+				if (playerData.getActiveDriveForm().equals(KingdomKeysReMind.MODID + ":" + StringsRM.darkMode))
+					return playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oblivionChain.get();
+				
+				if (playerData.getActiveDriveForm().equals(KingdomKeysReMind.MODID + ":" + StringsRM.light))
+					return playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oathkeeperChain.get();
+				
+				return playerData.getActiveDriveForm().equals(KingdomKeysReMind.MODID + ":" + StringsRM.twilight);
+			}
+		}
+    	return false;
     }
 }
