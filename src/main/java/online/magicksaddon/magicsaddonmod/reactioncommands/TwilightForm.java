0af2e1aefa82f7280dd6400c3ab9de40b42e2ad0
Abelatox <abel54225@gmail.com>
2024-04-23 18:03:43 +0200

    Fixed RC appearing

--- src/main/java/online/magicksaddon/magicsaddonmod/reactioncommands/TwilightForm.java
+++ src/main/java/online/magicksaddon/magicsaddonmod/reactioncommands/TwilightForm.java
@@ -1,72 +1,55 @@
 package online.magicksaddon.magicsaddonmod.reactioncommands;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.ThrowableProjectile;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import online.kingdomkeys.kingdomkeys.capability.IPlayerCapabilities;
 import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.kingdomkeys.kingdomkeys.driveform.DriveForm;
-import online.kingdomkeys.kingdomkeys.driveform.ModDriveForms;
 import online.kingdomkeys.kingdomkeys.item.ModItems;
-import online.kingdomkeys.kingdomkeys.lib.Strings;
 import online.kingdomkeys.kingdomkeys.reactioncommands.ReactionCommand;
 import online.magicksaddon.magicsaddonmod.KingdomKeysReMind;
 import online.magicksaddon.magicsaddonmod.capabilities.IGlobalCapabilitiesRM;
 import online.magicksaddon.magicsaddonmod.capabilities.ModCapabilitiesRM;
-import online.magicksaddon.magicsaddonmod.client.sound.ModSoundsRM;
-import online.magicksaddon.magicsaddonmod.driveform.DriveFormTwilight;
-import online.magicksaddon.magicsaddonmod.driveform.ModDriveFormsRM;
-import online.magicksaddon.magicsaddonmod.entity.reactioncommand.DarkMineEntity;
 import online.magicksaddon.magicsaddonmod.lib.StringsRM;
-import online.magicksaddon.magicsaddonmod.network.PacketHandlerRM;
 
 @Mod.EventBusSubscriber(modid = KingdomKeysReMind.MODID)
 public class TwilightForm extends ReactionCommand {
 
+	public TwilightForm(ResourceLocation registryName, boolean constantCheck) {
+		super(registryName, constantCheck);
+	}
 
-    public TwilightForm(ResourceLocation registryName, boolean constantCheck) {
-        super(registryName, constantCheck);
-    }
+	@Override
+	public void onUse(Player player, LivingEntity livingEntity, LivingEntity livingEntity1) {
+		if (conditionsToAppear(player, player)) {
+			IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
+			IGlobalCapabilitiesRM globalData = ModCapabilitiesRM.getGlobal(player);
+			playerData.setActiveDriveForm(KingdomKeysReMind.MODID + ":" + StringsRM.twilight);
+			if (playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oblivionChain.get()) {
+				playerData.setNewKeychain(new ResourceLocation(KingdomKeysReMind.MODID + ":" + StringsRM.twilight), new ItemStack(ModItems.oathkeeperChain.get()));
+			}
+			if (playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oathkeeperChain.get()) {
+				playerData.setNewKeychain(new ResourceLocation(KingdomKeysReMind.MODID + ":" + StringsRM.twilight), new ItemStack(ModItems.oblivionChain.get()));
+			}
+		}
+	}
 
-
-    @Override
-    public void onUse(Player player, LivingEntity livingEntity, LivingEntity livingEntity1) {
-        if (conditionsToAppear(player, player)) {
-            IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
-            IGlobalCapabilitiesRM globalData = ModCapabilitiesRM.getGlobal(player);
-            playerData.setActiveDriveForm(KingdomKeysReMind.MODID + ":" + StringsRM.twilight);
-            if (playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oblivionChain.get()) {
-                playerData.setNewKeychain(new ResourceLocation(KingdomKeysReMind.MODID + ":" + StringsRM.twilight), ModItems.oathkeeperChain.get());
-            }
-            if (playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oathkeeperChain.get()) {
-                playerData.setNewKeychain(new ResourceLocation(KingdomKeysReMind.MODID + ":" + StringsRM.twilight), ModItems.oblivionChain.get());
-            }
-        }
-    }
-
-
-    @Override
-    public boolean conditionsToAppear(Player player, LivingEntity livingEntity) {
-        IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
-        IGlobalCapabilitiesRM globalData = ModCapabilitiesRM.getGlobal(player);
-        if (playerData != null) {
-            if (playerData.getDriveFormLevel(KingdomKeysReMind.MODID+":"+StringsRM.darkMode) == 7 && playerData.getDriveFormLevel(KingdomKeysReMind.MODID+":"+StringsRM.light) == 7) {
-                //System.out.println(playerData.getEquippedKeychain(DriveForm.NONE));
-                //System.out.println(playerData.getEquippedKeychain(DriveForm.SYNCH_BLADE));
-                if (playerData.getActiveDriveForm().equals(KingdomKeysReMind.MODID+":"+StringsRM.darkMode)) {
-                    if (playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oblivionChain.get() && playerData.getEquippedKeychain(DriveForm.SYNCH_BLADE).getItem() == ModItems.oathkeeperChain.get()){
-                        return true;
-                    }
-                } else if (playerData.getActiveDriveForm().equals(KingdomKeysReMind.MODID+":"+StringsRM.light)) {
-                    if (playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oathkeeperChain.get() && playerData.getEquippedKeychain(DriveForm.SYNCH_BLADE).getItem() == ModItems.oblivionChain.get()){
-                        return true;
-                    }
-                }
-            }
-        }
-            return false;
-        }
-    }
+	@Override
+	public boolean conditionsToAppear(Player player, LivingEntity livingEntity) {
+		IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
+		IGlobalCapabilitiesRM globalData = ModCapabilitiesRM.getGlobal(player);
+		if (playerData != null) {
+			if (playerData.getDriveFormLevel(KingdomKeysReMind.MODID + ":" + StringsRM.darkMode) == 7 && playerData.getDriveFormLevel(KingdomKeysReMind.MODID + ":" + StringsRM.light) == 7) {
+				if (playerData.getActiveDriveForm().equals(KingdomKeysReMind.MODID + ":" + StringsRM.darkMode))
+					return playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oblivionChain.get() && playerData.getEquippedKeychain(DriveForm.SYNCH_BLADE).getItem() == ModItems.oathkeeperChain.get();
+				
+				if (playerData.getActiveDriveForm().equals(KingdomKeysReMind.MODID + ":" + StringsRM.light))
+					return playerData.getEquippedKeychain(DriveForm.NONE).getItem() == ModItems.oathkeeperChain.get() && playerData.getEquippedKeychain(DriveForm.SYNCH_BLADE).getItem() == ModItems.oblivionChain.get();
+			}
+		}
+		return false;
+	}
+}
