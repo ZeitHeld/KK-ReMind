d6aab7b36e317d13e60c311f64671c091e7ae518
Xephiro9999 <dreadbot8001@gmail.com>
2024-08-02 13:22:16 -0400

    Functional Renewal Block and Focus Block

--- src/main/java/online/remind/remind/mixin/GuardSkillMixin.java
+++ src/main/java/online/remind/remind/mixin/GuardSkillMixin.java
@@ -5,6 +5,8 @@ import online.kingdomkeys.kingdomkeys.capability.IPlayerCapabilities;
 import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.remind.remind.lib.StringsRM;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import yesman.epicfight.api.utils.AttackResult;
 import yesman.epicfight.skill.guard.GuardSkill;
@@ -14,10 +16,9 @@ import yesman.epicfight.world.entity.eventlistener.HurtEvent;
 @Mixin(GuardSkill.class)
 public class GuardSkillMixin {
 
-    public void dealEvent(PlayerPatch<?> playerpatch, HurtEvent.Pre event, boolean advanced, CallbackInfo ci) {
+    @Inject(method = "dealEvent", at = @At("TAIL"), remap = false)
+    public void dealEventInject(PlayerPatch<?> playerpatch, HurtEvent.Pre event, boolean advanced, CallbackInfo ci) {
 
-        event.setCanceled(true);
-        event.setResult(AttackResult.ResultType.BLOCKED);
         Player player = playerpatch.getOriginal();
         IPlayerCapabilities playerCapabilities = ModCapabilities.getPlayer(player);
 
@@ -27,13 +28,16 @@ public class GuardSkillMixin {
 
         if(playerCapabilities.isAbilityEquipped(StringsRM.renewalBlock)) {
             player.heal(player.getMaxHealth() * 0.05F);
-            System.out.println("Healed on Block!");
+            player.getFoodData().eat(3,3);
+            System.out.println("Healed for "+ player.getMaxHealth() * 0.05F +" on Block!");
 
         }
 
         if(playerCapabilities.isAbilityEquipped(StringsRM.focusBlock)) {
             playerCapabilities.addFocus(10);
             System.out.println("Focus Restored on Block!");
+            System.out.println(event.getDamageSource().getEntity());
+            
 
         }
 
